85ba136790b4973b4cc2a765e5bb01bacf5d1b20
Avas <avas.bajracharya@renegadeinsurance.com>
2024-07-14 18:34:19 +0545

    feat: separated controllers from main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
-@Controller()
-class AppController {
-  @Get()
-  getRootRoute() {
-    return "hello I am here";
-  }
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
+
+bootstrap();
